647d01e447897cb6af1bd2351527de669a584bda
Bisesh <bisesh.sitaula@gmail.com>
2022-08-22 22:02:09 +0100

    Removed dead code

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Hero from "./Hero/Hero";
-import Navbar from "./navbar/Navbar";
 
 const MainContent = () => {
   return (
     <div>
-      {/* <Navbar /> */}
       <Hero />
     </div>
   );
