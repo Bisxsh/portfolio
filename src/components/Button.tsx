24af4efa2d1ca18fe720712b8bb4ddb602b0014d
Bisesh <bisesh.sitaula@gmail.com>
2022-08-22 22:10:50 +0100

    Implemented framer motion button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,48 +1,73 @@
 import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 export function Button(props: any) {
-  return <ButtonBase className={props.setClassName}>{props.text}</ButtonBase>;
+  return (
+    <ButtonBase className={props.setClassName}>
+      <motion.button
+        className="button"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        {props.text}
+      </motion.button>
+    </ButtonBase>
+  );
 }
 
 const ButtonBase = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-inline: 10px;
-  cursor: pointer;
+  .button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-inline: 10px;
+    cursor: pointer;
 
-  font-weight: 400;
-  font-size: var(--font-sm);
-  color: var(--color-primary);
+    font-weight: 400;
+    font-size: var(--font-sm);
+    color: var(--color-primary);
 
-  width: 70px;
-  height: 30px;
+    width: 70px;
+    height: 30px;
 
-  border: 2px solid #457b9d;
-  border-radius: 10px;
+    border: 2px solid #457b9d;
+    border-radius: 10px;
 
-  transition: all 0.2s ease-in-out;
-  transition-property: all;
-  transition-duration: 0.2s;
-  transition-timing-function: ease-in-out;
-  transition-delay: 0s;
-  -webkit-transition: all 0.2s ease-in-out;
+    transition: all 0.2s ease-in-out;
+    transition-property: all;
+    transition-duration: 0.2s;
+    transition-timing-function: ease-in-out;
+    transition-delay: 0s;
+    -webkit-transition: all 0.2s ease-in-out;
 
-  &:hover {
-    background-color: var(--color-primary);
-    color: var(--color-code-bg);
+    &:hover {
+      background-color: var(--color-primary);
+      color: var(--color-code-bg);
+    }
   }
 `;
 
 export function LargeButton(props: any) {
-  return <LargeButtonComponent>{props.text}</LargeButtonComponent>;
+  return (
+    <LargeButtonComponent>
+      <motion.button
+        className="button large"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        {props.text}
+      </motion.button>
+    </LargeButtonComponent>
+  );
 }
 
 const LargeButtonComponent = styled(ButtonBase)`
-  width: 320px;
-  height: 60px;
-  border-radius: 20px;
-  font-size: var(--font-lg);
-  margin: 0;
+  .large {
+    width: 320px;
+    height: 60px;
+    border-radius: 20px;
+    font-size: var(--font-lg);
+    margin: 0;
+  }
 `;
