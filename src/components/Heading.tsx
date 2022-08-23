50c52f9fdb75c51db9c6164cfa749a3e10b233a6
Bisesh <bisesh.sitaula@gmail.com>
2022-08-23 09:47:10 +0100

    Created heading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+const Heading = (props: any) => {
+  let initial: any = {
+    opacity: 0,
+    x: props.left ? -200 : 200,
+  };
+
+  return (
+    <HeadingWrapper
+      isLeft={props.left}
+      className={`${props.left ? "left" : ""}`}
+    >
+      <div className={`heading ${props.left ? "left" : "right"}`}>
+        <motion.h1
+          className={`heading ${props.left ? "left" : ""}`}
+          initial={initial}
+          whileInView={{ x: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{
+            default: {
+              duration: 0.7,
+            },
+          }}
+        >
+          {props.text}
+        </motion.h1>
+      </div>
+    </HeadingWrapper>
+  );
+};
+
+const HeadingWrapper: any = styled.div`
+  width: 100%;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  .heading {
+    font-size: 128px;
+    line-height: 192px;
+
+    color: rgba(29, 53, 87, 0.25);
+  }
+
+  .left {
+    justify-content: start;
+  }
+
+  .right {
+    justify-content: end;
+  }
+`;
+
+export default Heading;
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Heading from "./heading";
 import Hero from "./Hero/Hero";
 
 const MainContent = () => {
   return (
     <div>
       <Hero />
+      <Heading text="About Me" left={true} />
+      <Heading text="Skills" left={false} />
     </div>
   );
 };
