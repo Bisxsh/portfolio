933aba0713549fd33291b46afa01b072fcc421d9
Bisesh <bisesh.sitaula@gmail.com>
2022-08-23 19:57:28 +0100

    Fixed todo image

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -14,7 +14,7 @@ const Projects = () => {
         imagePath={todoImage}
         name="To-Do List"
         description="A simple to-do list app I made when learning Angular and CSS animations"
-        technologies="Angular Typescript Material-UI"
+        technologies="Angular · Typescript · Material-UI"
         github="https://github.com/Bisxsh/angular-todo-list"
         figma="https://www.figma.com/file/GQTxnhir2TgDUJDulqsMWV/Todo-list?node-id=0%3A1"
         dribbble="https://dribbble.com/shots/18943466-To-do-List"
@@ -24,7 +24,7 @@ const Projects = () => {
         imagePath={todoImage}
         name="To-Do List"
         description="A simple to-do list app I made when learning Angular and CSS animations"
-        technologies="Angular Typescript Material-UI"
+        technologies="Angular · Typescript · Material-UI"
         github="https://github.com/Bisxsh/angular-todo-list"
         figma="https://www.figma.com/file/GQTxnhir2TgDUJDulqsMWV/Todo-list?node-id=0%3A1"
         dribbble="https://dribbble.com/shots/18943466-To-do-List"
@@ -34,7 +34,7 @@ const Projects = () => {
         imagePath={pomodoroImage}
         name="Pomodoro Timer"
         description="A minimilistic Pomodoro Timer app I created when learning React Native"
-        technologies="React Native Typescript Expo"
+        technologies="React Native · Typescript · Expo"
         github="https://github.com/Bisxsh/angular-todo-list"
         figma="https://www.figma.com/file/GQTxnhir2TgDUJDulqsMWV/Todo-list?node-id=0%3A1"
         dribbble="https://dribbble.com/shots/18943466-To-do-List"
