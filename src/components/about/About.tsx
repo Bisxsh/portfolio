abd114ab7d6703c72fcf34082c3e4b274f0a4838
Bisesh <bisesh.sitaula@gmail.com>
2022-08-23 12:07:22 +0100

    Moved Sphere into seperate component

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,57 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { TagSphere } from "react-tag-sphere";
 import styled from "styled-components";
-import { getSkillsObjects } from "./components/SphereItem";
+import { getSkillsObjects, TagSphereComponent } from "./components/SphereItem";
 
 const About = () => {
-  const skillsObjects = getSkillsObjects();
-  //   const [imageIndexes, setImageIndexes] = useState(getThreeRandomIndexes());
-  //   const [firstNum, setFirstNum] = useState(imageIndexes[0]);
-  //   const [tags, setTags] = useState(getTags());
-
-  function getTags() {
-    return skillsObjects.map(s => s.imageComponent);
-    // TODO make animate change between text and image without resetting sphere position
-    // return skillsObjects.map(s => {
-    //   if (imageIndexes.indexOf(skillsObjects.indexOf(s)) === -1)
-    //     return s.textComponent;
-    //   return s.imageComponent;
-    // });
-  }
-
-  //   function getThreeRandomIndexes(): number[] {
-  //     let indexes: number[] = [];
-  //     do {
-  //       let num = Math.floor(Math.random() * (skillsObjects.length - 1));
-  //       if (indexes.indexOf(num) === -1) indexes.push(num);
-  //     } while (indexes.length < 3);
-  //     return indexes;
-  //   }
-
-  //   useEffect(() => {
-  //     const interval = setInterval(() => {
-  //       let indexes: number[] = imageIndexes;
-  //       let num: number;
-
-  //       do {
-  //         num = Math.floor(Math.random() * (skillsObjects.length - 1));
-  //       } while (indexes.indexOf(num) !== -1);
-
-  //       indexes.shift();
-  //       indexes.push(num);
-  //       setImageIndexes(indexes);
-  //       setFirstNum(indexes[0]);
-  //     }, 3000);
-
-  //     return () => clearInterval(interval);
-  //   }, []);
-
-  //   useEffect(() => {
-  //     setTags(getTags);
-  //     console.log("Here");
-  //     console.log(imageIndexes);
-  //   }, [firstNum]);
-
   return (
     <AboutWrapper>
       <AboutText>
@@ -66,24 +18,7 @@ const About = () => {
         </p>
       </AboutText>
       <SphereWrapper>
-        <TagSphere
-          style={{
-            color: "#457b9d",
-            fontWeight: "400",
-            fontSize: "1.2rem",
-            fontFamily: "Poppins",
-          }}
-          className="about--sphere"
-          blur={true}
-          blurMultiplier={0.2}
-          keepRollingAfterMouseOut={false}
-          userSelect={true}
-          initialSpeed={2}
-          maxSpeed={2}
-          fullWidth={true}
-          fullHeight={true}
-          tags={getTags()}
-        />
+        <TagSphereComponent />
       </SphereWrapper>
     </AboutWrapper>
   );
--- src/components/about/components/SphereItem.tsx
+++ src/components/about/components/SphereItem.tsx
@@ -1,5 +1,5 @@
-import { StaticImage } from "gatsby-plugin-image";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TagSphere } from "react-tag-sphere";
 import {
   TypescriptIcon,
   JavascriptIcon,
@@ -50,3 +50,69 @@ export function getSkillsObjects(): SphereItem[] {
   }
   return list;
 }
+
+export const TagSphereComponent = () => {
+  //TODO fix dynamic swapping of text and image without resetting sphere position
+  const skillsObjects = getSkillsObjects();
+  // const [imageIndexes, setImageIndexes] = useState(getThreeRandomIndexes());
+  // const [firstNum, setFirstNum] = useState(imageIndexes[0]);
+  // const [tags, setTags] = useState(getTags());
+
+  // function getThreeRandomIndexes(): number[] {
+  //   let indexes: number[] = [];
+  //   do {
+  //     let num = Math.floor(Math.random() * (skillsObjects.length - 1));
+  //     if (indexes.indexOf(num) === -1) indexes.push(num);
+  //   } while (indexes.length < 3);
+  //   return indexes;
+  // }
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     let indexes: number[] = imageIndexes;
+  //     let num: number;
+
+  //     do {
+  //       num = Math.floor(Math.random() * (skillsObjects.length - 1));
+  //     } while (indexes.indexOf(num) !== -1);
+
+  //     indexes.shift();
+  //     indexes.push(num);
+  //     setImageIndexes(indexes);
+  //     setFirstNum(indexes[0]);
+  //   }, 3000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  // useEffect(() => {
+  //   setTags(getTags);
+  //   console.log("Here");
+  //   console.log(imageIndexes);
+  // }, [firstNum]);
+
+  function getTags() {
+    return skillsObjects.map(s => s.imageComponent);
+  }
+
+  return (
+    <TagSphere
+      style={{
+        color: "#457b9d",
+        fontWeight: "400",
+        fontSize: "1.2rem",
+        fontFamily: "Poppins",
+      }}
+      className="about--sphere"
+      blur={true}
+      blurMultiplier={0.2}
+      keepRollingAfterMouseOut={false}
+      userSelect={true}
+      initialSpeed={2}
+      maxSpeed={2}
+      fullWidth={true}
+      fullHeight={true}
+      tags={getTags()}
+    />
+  );
+};
